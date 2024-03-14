8c006dcc89b4b8d5e5e6ced6d80a20ba2eaf2476
Shubhro Bhowmik <shubhrobhowmik@gmail.com>
2024-03-14 20:26:46 +0530

    style: add some tailwind classes
    add some tailwind classes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
-			<h1 className="underline">Vite + React ...</h1>
+			<p className="underline font-thin">Vite + React ...</p>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
 				<p>
